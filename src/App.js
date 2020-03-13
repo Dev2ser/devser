d4f058ba16e60b5e6d9566b8c0b68f4f2bbd5975
Robert01 <robow13.8hr@gmail.com>
2020-03-13 21:00:29 +0300

    tests

--- src/App.js
+++ src/App.js
@@ -1,41 +1,39 @@
-import React, { Component } from "react";
-import "./App.css";
-import fire from "./config/Fire";
-import Home from "./components/Dashboard";
-import SignIn from "./components/SignIn";
-import Dashboard from "./components/ui/dashboard/Dashboard";
+import React, { Component } from 'react'
+import './App.css'
+import fire from './config/Fire'
+import Admin from './components/Admin'
+import SignIn from './components/SignIn'
+import Dashboard from './components/ui/dashboard/Dashboard'
 
 class App extends Component {
   constructor() {
-    super();
+    super()
     this.state = {
       user: null
-    };
-    this.authListener = this.authListener.bind(this);
+    }
+    this.authListener = this.authListener.bind(this)
   }
 
   componentDidMount() {
-    this.authListener();
+    this.authListener()
   }
 
   authListener() {
     fire.auth().onAuthStateChanged(user => {
-      console.log(user);
+      console.log(user)
       if (user) {
-        this.setState({ user });
-        localStorage.setItem("user", user.uid);
+        this.setState({ user })
+        sessionStorage.setItem('user', user.uid)
       } else {
-        this.setState({ user: null });
-        localStorage.removeItem("user");
+        this.setState({ user: null })
+        sessionStorage.removeItem('user')
       }
-    });
+    })
   }
 
   render() {
-    return (
-      <div className="App">{this.state.user ? <Dashboard /> : <SignIn />}</div>
-    );
+    return <div className="App">{this.state.user ? <Admin /> : <SignIn />}</div>
   }
 }
 
-export default App;
+export default App
