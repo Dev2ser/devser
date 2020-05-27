42f08f8461520045dbb087c365ed5108650f1517
Robert01 <robow13.8hr@gmail.com>
2020-05-27 07:11:58 +0300

    v2.2

--- src/config/base.js
+++ src/config/base.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const config = {
-  apiKey: "AIzaSyBFjYqbNvGVarL7OI8nBO7GuHqusEBb408",
-  authDomain: "lemonco-bff8d.firebaseapp.com",
-  databaseURL: "https://lemonco-bff8d.firebaseio.com",
-  projectId: "lemonco-bff8d",
-  storageBucket: "lemonco-bff8d.appspot.com",
-  messagingSenderId: "820860844282",
-  appId: "1:820860844282:web:2bbf9f77e2999ac59d3143",
-  measurementId: "G-8PVSZ6GMDZ"
+  apiKey: "AIzaSyCK5MmJrfVw-ZxmnuBDle2qXHmCYFG5J1Q",
+  authDomain: "console-devser.firebaseapp.com",
+  databaseURL: "https://console-devser.firebaseio.com",
+  projectId: "console-devser",
+  storageBucket: "console-devser.appspot.com",
+  messagingSenderId: "644144043418",
+  appId: "1:644144043418:web:405efe9f94f6111ff31623",
+  measurementId: "G-MP57LVRW7B",
 };
 // Initialize Firebase
 const fire = firebase.initializeApp(config);
