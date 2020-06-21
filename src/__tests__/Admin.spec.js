c523f60213de7d35c6aeb5c37808f5b70432ee43
Robert01 <robow13.8hr@gmail.com>
2020-06-22 00:12:36 +0300

    test files fixes

--- src/__tests__/Admin.spec.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import AdminPanel from "./../components/Admin";
-
-it("renders AdminPanel Correctly", () => {
-  const { queryByTestId } = render(<AdminPanel />);
-
-  expect(queryByTestId("logout-btn")).toBeTruthy();
-});
