c518cdfac9dfe37712a92ff7b2e19a1513c23f75
devser <robow13.8hr@gmail.com>
2020-10-16 21:43:48 +0300

    testing travis

---
+++ build/precache-manifest.76adefcdf7222187bc66bbf7a97b6678.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70358ff95634c5b6803a6ef787af62ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2c9691fdcb6fc9946b2",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "5e83a8556d408f6361b2",
+    "url": "/static/css/main.60e529c4.chunk.css"
+  },
+  {
+    "revision": "c2c9691fdcb6fc9946b2",
+    "url": "/static/js/2.17b53afb.chunk.js"
+  },
+  {
+    "revision": "b9dc90b138da6790f1e964659fde28b3",
+    "url": "/static/js/2.17b53afb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e83a8556d408f6361b2",
+    "url": "/static/js/main.d9336201.chunk.js"
+  },
+  {
+    "revision": "ab049e8cf97c47d1c962",
+    "url": "/static/js/runtime-main.2b10bf74.js"
+  },
+  {
+    "revision": "b8acb54272c18742d66edbfb6bcd2d08",
+    "url": "/static/media/PageNotFound.b8acb542.png"
+  },
+  {
+    "revision": "62aeffa448352164c90789bbe540d158",
+    "url": "/static/media/about.62aeffa4.svg"
+  },
+  {
+    "revision": "fcce812f277d57afede8bf48c34f149a",
+    "url": "/static/media/google.fcce812f.jpg"
+  },
+  {
+    "revision": "f620ca26b6fd989db6d270c2f0e34541",
+    "url": "/static/media/ladder-700px.f620ca26.png"
+  },
+  {
+    "revision": "3498c36323acf5c0682fefe0f933b110",
+    "url": "/static/media/logo.3498c363.svg"
+  },
+  {
+    "revision": "7e145c9eb60da95379f070be9920eca5",
+    "url": "/static/media/twitter.7e145c9e.svg"
+  }
+]);
