e0282fbb54c2883f0a0a0847b606b206fb382f64
devser <robow13.8hr@gmail.com>
2020-10-22 00:59:09 +0300

    removed routes (impermanent).

--- src/routes/index.js
+++
@@ -1,6 +0,0 @@
-export { BaseRoute } from './Authentication/BaseRoute';
-export { SignInRoute } from './Authentication/SignInRoute';
-export { SignUpRoute } from './Authentication/SignUpRoute';
-export { AccountRoute } from './Personalized/AccountRoute';
-export { SubjectsRoute } from './Personalized/SubjectsRoute';
-export { PrivateRoute } from './PrivateRoute';
