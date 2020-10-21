e9e13e95796e5fed44394b866ababeaa1198af54
devser <robow13.8hr@gmail.com>
2020-10-21 20:01:31 +0300

    Generated a changelog, other minor changes

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-var express = require('express')
-var app = express()
+const express = require('express');
+const app = express();
 
-app.use(express.static(__dirname + '/'))
+app.use(express.static(__dirname + '/build'));
 
-app.listen(process.env.PORT || 8080)
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`server listening at http://localhost:${PORT}`);
+});
